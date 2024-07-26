325b4dae68e983a3dba62f14ca8478e59eb9b1b0
wittyBoffin <maxcore1120@gmail.com>
2024-07-26 14:17:55 +0200

    add wallet connection modal

--- src/app/components/WalletConnectMolal.tsx
+++ src/app/components/WalletConnectMolal.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useLayoutEffect } from "react";
+import Image from "next/image";
 import Modal from "react-modal";
 
 type WalletModalProps = {
@@ -14,6 +15,7 @@ const customStyles = {
     bottom: "auto",
     marginRight: "-50%",
     transform: "translate(-50%, -50%)",
+    backgroundColor: "#FF5400",
   },
 };
 
@@ -25,9 +27,48 @@ const WalletConnectMolal = ({ isOpen, onClose }: WalletModalProps) => {
         onRequestClose={() => onClose(false)}
         style={customStyles}
         ariaHideApp={false}
+        className="relative transform overflow-hidden bg-[#FF5400] border-4 border-black ring-4 ring-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6"
         contentLabel="Example Modal"
       >
-        this is modal
+        <button
+          //   onClick={}
+          className="flex items-center justify-center gap-x-4 py-3 px-4 mx-auto mb-5 text-white text-[15px] h-[48px] min-w-[250px] border-4 border-black ring-4 ring-white hover:scale-105 duration-200 cursor-pointer"
+        >
+          <Image
+            src="/magic-eden-logo.png"
+            alt="magiceden"
+            width={20}
+            height={20}
+            priority
+          />{" "}
+          magiceden
+        </button>
+        <button
+          //   onClick={() => setIsOpen(!isOpen)}
+          className="flex items-center justify-center gap-x-4 py-3 px-4 mx-auto mb-5 text-white text-[15px] h-[48px] min-w-[250px] border-4 border-black ring-4 ring-white hover:scale-105 duration-200 cursor-pointer"
+        >
+          <Image
+            src="/xverse-logo.png"
+            alt="xverse"
+            width={20}
+            height={20}
+            priority
+          />{" "}
+          xverse
+        </button>
+        <button
+          //   onClick={() => setIsOpen(!isOpen)}
+          className="flex items-center justify-center gap-x-4 py-3 px-4 mx-auto text-white text-[15px] h-[48px] min-w-[250px] border-4 border-black ring-4 ring-white hover:scale-105 duration-200 cursor-pointer"
+        >
+          <Image
+            src="/unisat.svg"
+            alt="unisat"
+            width={20}
+            height={20}
+            priority
+          />{" "}
+          unisat
+        </button>
       </Modal>
     </div>
   );
