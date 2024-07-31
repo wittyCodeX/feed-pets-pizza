efd7106ca51293177acda0a9a0587392760d8cdc
wittyBoffin <maxcore1120@gmail.com>
2024-07-31 17:47:00 +0200

    update setStorage code as typescript

--- src/app/utils/setStorage.ts
+++ src/app/utils/setStorage.ts
@@ -1,3 +1,8 @@
-export const setStorage = (key: string, value: any) => {
-    localStorage.setItem(key, JSON.stringify(value));
+export const setStorage = <T>(key: string, value: T): void => {
+    try {
+        const serializedValue = JSON.stringify(value);
+        localStorage.setItem(key, serializedValue);
+    } catch (e) {
+        console.error(`Error serializing value for localStorage key "${key}":`, e);
+    }
 }
