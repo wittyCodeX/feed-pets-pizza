695b7b50d464fbe8b21acb60a40126f3a25fdebd
wittyCodeX <maxcore1120@gmail.com>
2024-08-16 22:06:11 +0200

    optimized codebase

--- src/app/components/WalletConnectMolal.tsx
+++ src/app/components/WalletConnectMolal.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useLayoutEffect } from "react";
+import React from "react";
 import Image from "next/image";
 import Modal from "react-modal";
 import { AddressPurpose, request } from "sats-connect";
